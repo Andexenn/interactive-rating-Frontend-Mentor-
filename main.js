ae9b43a33f6ed7b41ca09f5a52355cee6280cb2f
ryan <phanhuynhhung0707@gmail.com>
2025-06-02 09:31:36 +0700

    feat: finish how to get point and swap card in main.js

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const ratingPointList = document.querySelectorAll("li");
 const firstBGColor = "hsla(217, 12%, 63%, 0.1)";
 const secondBGColor = "hsl(0, 100%, 100%)";
 const button = document.querySelector(".btn");
+const container = document.querySelectorAll(".container");
+const point = document.querySelector(".point");
 let selectedPoint = -1;
+// console.log(ratingPointList);
 
 for(let i = 0; i < ratingPointList.length; ++i){
     const currPoint = ratingPointList[i];
     currPoint.addEventListener("click", () =>{
-        if(currPoint.style.backgroundColor === firstBGColor){
-            currPoint.style.backgroundColor = secondBGColor;
-            if(selectedPoint !== -1){
-                ratingPointList[selectedPoint].style.backgroundColor = firstBGColor;
-            }
+        if(selectedPoint === -1){
             selectedPoint = i;
+            currPoint.classList.add("active");
         }
         else{
-            currPoint.style.backgroundColor = firstBGColor;
-            selectedPoint = -1;
+            ratingPointList[selectedPoint].classList.remove("active");
+            currPoint.classList.add("active");
+            selectedPoint = i;
         }
     })
 }
@@ -24,6 +25,9 @@ for(let i = 0; i < ratingPointList.length; ++i){
 button.addEventListener("click", () =>{
     if(selectedPoint !== -1){
         //? them bo the active
+        container[0].classList.remove("active");
+        container[1].classList.add("active");
+        point.textContent = `${selectedPoint + 1}`;
         //? lay diem de qua ben the thank you 
     }
 })
